3b8416c7debefe21256a23302d64fbba4b140691
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-11 17:08:14 +0200

    created form

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var dataTF: UITextField!
+    
+    @IBOutlet weak var monthTF: UITextField!
 
+    @IBOutlet var yearTF: UITextField!
+    
+    @IBOutlet var resultLabel: UIView!
+
+    @IBAction func findDay() {
+        
+    }
 
 }
 

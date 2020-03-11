8232b9721ad1509dbfff8fe7bb8a370034e611ee
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-11 18:53:22 +0200

    complet WeekdayFinder

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -11,15 +11,47 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var dataTF: UITextField!
-    
     @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
 
-    @IBOutlet var yearTF: UITextField!
-    
-    @IBOutlet var resultLabel: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
 
-    @IBAction func findDay() {
+    @IBAction func findWeekDay() {
         
+        // Достаем календарь
+        let calendar = Calendar.current
+        
+        // Создаем пустой компонент даты
+        var dateComponents = DateComponents()
+        
+        // Инструкцией guard проверяем данные и достаем из инпутов
+        guard let day = dataTF.text,
+              let month = monthTF.text,
+              let year = yearTF.text
+            else { return }
+        
+        // Пушим данные в контейнер dateComponents
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        // Сново проверяем данные и преобразуим их в дату
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        // Задаем нужный формат вывода даты (достаем день недели)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        // Приобразуем дату в нужный строковый формат
+        let weekday = dateFormatter.string(from: date)
+        
+        // Помещаем данные в лейбл
+        resultLabel.text = weekday
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }

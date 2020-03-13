d661a7eaab20421fcc88959172727b653185cd78
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-13 15:10:40 +0200

    PassDataProject

---
+++ PassData/PassData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData
+//
+//  Created by Artem Panasenko on 13.03.2020.
+//  Copyright © 2020 Artem Panasenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UILTextField!
+    @IBOutlet weak var passLabel: UILabel!
+    
+    @IBAction func
+
+}
+

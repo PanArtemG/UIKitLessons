cdb04edfa708b6ff75c18c4fae50b063067530a1
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-03-14 13:07:31 +0200

    UISlider

--- UISegmentedControl/UISegmentedControl/ViewController.swift
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -12,18 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var segmenedControl: UISegmentedControl!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.isHidden = true
+        slider.value = 1
+        
+        label.text = String(slider.value)
         label.font = label.font.withSize(35)
         label.textAlignment = .center
         label.numberOfLines = 2
         
         segmenedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .white
     }
 
     @IBAction func choiceSegment(_ sender: UISegmentedControl) {
@@ -44,9 +53,14 @@ class ViewController: UIViewController {
             print("ERROR!!!")
         
         }
+    }
+    @IBAction func sliderAction(_ sender: UISlider) {
         
+        label.text = String(sender.value)
         
-        
+        let bgc = self.view.backgroundColor
+        // withAlphaComponent(CGFloat(sender.value)) - свойство прозрачности фона екрана
+        self.view.backgroundColor = bgc?.withAlphaComponent(CGFloat(sender.value))
     }
     
 }
